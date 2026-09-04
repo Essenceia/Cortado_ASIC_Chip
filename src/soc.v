3a45dee0f4760dfbd305b7ef52f2b102426c01c1
Julia Desmazes - ws <julia.desmazes@gmail.com>
2026-09-03 20:24:46 -0500

    wire up debug infra

--- src/soc.v
+++ src/soc.v
@@ -5,13 +5,21 @@
 `default_nettype none
 
 module soc #(
-	parameter [3:0]  TAP_VERSION_NUM = 4'd3, // tapeout 3 featuring a custom jtag tap
-	parameter [15:0] TAP_PART_NUM = 16'hbeef,
-	parameter [10:0] TAP_MANIFACTURE_ID = 11'h6b, // continuing the inside joke
+	parameter W_PADDR         = 9,
+	parameter DTMCS_IDLE_HINT = 3'd4,
 	`include "hazard3_config.vh"
 ) (
 	input wire               clk,
 	input wire               rst_n,
+
+	// from jtag tap
+	input  wire                  tck_i,
+	input  wire                  trst_n_i,
+	input  wire                  dtm_wen_i,
+	input  wire                  dtm_ren_i,
+	input  wire                  dtm_sel_dmi_ndtmcs_i, 
+	input  wire [W_DR_SHIFT-1:0] dtm_wdata_i,
+	output wire [W_DR_SHIFT-1:0] dtm_rdata_o,
 
 	// AHB5 Master port
 	output reg  [W_ADDR-1:0]  haddr_o,
@@ -29,23 +37,6 @@ module soc #(
 	output wire [W_DATA-1:0]  hwdata_o,
 	input  wire [W_DATA-1:0]  hrdata_i,
 
-	// Debugger run/halt control
-	input  wire               dbg_req_halt_i,
-	input  wire               dbg_req_halt_on_reset_i,
-	input  wire               dbg_req_resume_i,
-	output wire               dbg_halted_o,
-	output wire               dbg_running_o,
-	// Debugger access to data0 CSR
-	input  wire [W_DATA-1:0]  dbg_data0_rdata_i,
-	output wire [W_DATA-1:0]  dbg_data0_wdata_o,
-	output wire               dbg_data0_wen_o,
-	// Debugger instruction injection
-	input  wire [W_DATA-1:0]  dbg_instr_data_i,
-	input  wire               dbg_instr_data_vld_i,
-	output wire               dbg_instr_data_rdy_o,
-	output wire               dbg_instr_caught_exception_o,
-	output wire               dbg_instr_caught_ebreak_o,
-
 	// Level-sensitive interrupt sources
 	input wire [NUM_IRQS-1:0] irq_i,       // -> mip.meip
 	input wire                soft_irq_i,  // -> mip.msip
@@ -62,9 +53,9 @@ wire dbg_sbus_rdy_unused;
 wire dbg_sbus_err_unused; 
 wire [W_DATA-1:0] dbg_sbus_rdata_unused;
 
-reg rst_d1_q; 
+reg rst_core_n; 
 always @(posedge clk) 
-	rst_d1_q <= rst_n; 
+	rst_core_n <= rst_n; 
 
 `ifdef EXTERNAL_REGILE
 // external regfile
@@ -75,12 +66,11 @@ reg  [W_DATA-1:0]    rf_rdata2;
 wire [W_REGADDR-1:0] rf_waddr; 
 wire [W_DATA-1:0]    rf_wdata;
 wire                 rf_wen;
-
 hazard3_regfile_1w2r #(
 `include "hazard3_config_inst.vh"
 ) regs (
 	.clk    (clk),
-	.rst_n  (rst_d1_q),
+	.rst_n  (rst_core_n),
 	// On downstream stall, we feed D's addresses back into regfile
 	// so that output does not change.
 	// GF180MCU: use fine rather than coarse predecode as the we rely on the
@@ -95,9 +85,138 @@ hazard3_regfile_1w2r #(
 );
 `endif // EXTERNAL_REGFILE
 
-// JTAG 
-localparam [31:0] IDCODE = {VERSION_NUM, PART_NUM, MANIFACTURE_ID, 1'b1};
- 
+// jtag tap can force hard reset
+wire dmihardreset_req;
+wire assert_dmi_reset = ~rst_core_n | dmihardreset_req;
+
+reset_sync dmi_reset_sync_u (
+	.clk       (clk),
+	.rst_n_in  (!assert_dmi_reset),
+	.rst_n_out (rst_n_dmi)
+);
+
+// JTAG
+wire              dmi_psel;
+wire              dmi_penable;
+wire              dmi_pwrite;
+wire [8:0]        dmi_paddr;
+wire [31:0]       dmi_pwdata;
+wire [31:0]       dmi_prdata;
+wire              dmi_pready;
+wire              dmi_pslverr;
+
+hazard3_jtag_dtm_core #(
+	.DTMCS_IDLE_HINT (DTMCS_IDLE_HINT),
+	.W_PADDR(W_PADDR)
+) dtm_core (
+	.tck               (tck),
+	.trst_n            (trst_n),
+	.clk_dmi           (clk),
+	.rst_n_dmi         (rst_core_n),
+
+	.dmihardreset_req  (dmihardreset_req),
+
+	.dr_wen            (dtm_wen_i),
+	.dr_ren            (dtm_ren_i),
+	.dr_sel_dmi_ndtmcs (dtm_sel_dmi_ndtmcs_i),
+	.dr_wdata          (dtm_wdata_i),
+	.dr_rdata          (dtm_rdata_o),
+
+	.dmi_psel          (dmi_psel),
+	.dmi_penable       (dmi_penable),
+	.dmi_pwrite        (dmi_pwrite),
+	.dmi_paddr         (dmi_paddr[W_PADDR-1:2]),
+	.dmi_pwdata        (dmi_pwdata),
+	.dmi_prdata        (dmi_prdata),
+	.dmi_pready        (dmi_pready),
+	.dmi_pslverr       (dmi_pslverr)
+);
+assign dmi_paddr[1:0] = 2'b00;
+
+// Debug Module
+localparam N_HARTS = 1;
+localparam XLEN = 32;
+
+wire                      sys_reset_req;
+wire                      sys_reset_done;
+wire [N_HARTS-1:0]        hart_reset_req;
+wire [N_HARTS-1:0]        hart_reset_done;
+
+wire [N_HARTS-1:0]        hart_req_halt;
+wire [N_HARTS-1:0]        hart_req_halt_on_reset;
+wire [N_HARTS-1:0]        hart_req_resume;
+wire [N_HARTS-1:0]        hart_halted;
+wire [N_HARTS-1:0]        hart_running;
+
+wire [N_HARTS*XLEN-1:0]   hart_data0_rdata;
+wire [N_HARTS*XLEN-1:0]   hart_data0_wdata;
+wire [N_HARTS-1:0]        hart_data0_wen;
+
+wire [N_HARTS*XLEN-1:0]   hart_instr_data;
+wire [N_HARTS-1:0]        hart_instr_data_vld;
+wire [N_HARTS-1:0]        hart_instr_data_rdy;
+wire [N_HARTS-1:0]        hart_instr_caught_exception;
+wire [N_HARTS-1:0]        hart_instr_caught_ebreak;
+
+wire [31:0]               sbus_addr;
+wire                      sbus_write;
+wire [1:0]                sbus_size;
+wire                      sbus_vld;
+wire                      sbus_rdy;
+wire                      sbus_err;
+wire [31:0]               sbus_wdata;
+wire [31:0]               sbus_rdata;
+
+hazard3_dm #(
+	.N_HARTS      (N_HARTS),
+	.HAVE_SBA     (0),
+	.NEXT_DM_ADDR (0)
+) dm (
+	.clk                         (clk),
+	.rst_n                       (rst_n),
+
+	.dmi_psel                    (dmi_psel),
+	.dmi_penable                 (dmi_penable),
+	.dmi_pwrite                  (dmi_pwrite),
+	.dmi_paddr                   (dmi_paddr),
+	.dmi_pwdata                  (dmi_pwdata),
+	.dmi_prdata                  (dmi_prdata),
+	.dmi_pready                  (dmi_pready),
+	.dmi_pslverr                 (dmi_pslverr),
+
+	.sys_reset_req               (sys_reset_req),
+	.sys_reset_done              (sys_reset_done),
+	.hart_reset_req              (hart_reset_req),
+	.hart_reset_done             (hart_reset_done),
+
+	.hart_req_halt               (hart_req_halt),
+	.hart_req_halt_on_reset      (hart_req_halt_on_reset),
+	.hart_req_resume             (hart_req_resume),
+	.hart_halted                 (hart_halted),
+	.hart_running                (hart_running),
+
+	.hart_data0_rdata            (hart_data0_rdata),
+	.hart_data0_wdata            (hart_data0_wdata),
+	.hart_data0_wen              (hart_data0_wen),
+
+	.hart_instr_data             (hart_instr_data),
+	.hart_instr_data_vld         (hart_instr_data_vld),
+	.hart_instr_data_rdy         (hart_instr_data_rdy),
+	.hart_instr_caught_exception (hart_instr_caught_exception),
+	.hart_instr_caught_ebreak    (hart_instr_caught_ebreak),
+
+	.sbus_addr                   (sbus_addr),
+	.sbus_write                  (sbus_write),
+	.sbus_size                   (sbus_size),
+	.sbus_vld                    (sbus_vld),
+	.sbus_rdy                    (sbus_rdy),
+	.sbus_err                    (sbus_err),
+	.sbus_wdata                  (sbus_wdata),
+	.sbus_rdata                  (sbus_rdata)
+);
+
+
+// core  
 hazard3_cpu_1port #(
 	// These must have the values given here for you to end up with a useful SoC:
 	.RESET_VECTOR    (32'h0000_0040),
@@ -149,7 +268,7 @@ hazard3_cpu_1port #(
 ) cpu (
 	.clk                        (clk),
 	.clk_always_on              (clk),
-	.rst_n                      (rst_d1_q),
+	.rst_n                      (rst_core_n),
 
 	.pwrup_req                  (pwrup_req),
 	.pwrup_ack                  (pwrup_req),   // Tied back
@@ -186,30 +305,35 @@ hazard3_cpu_1port #(
 	.fence_i_vld                (fence_i_vld_unused),
 	.fence_d_vld                (fence_d_vld_unused),
 	.fence_rdy                  (1'b1),
+	
+	// Debugger run/halt control
+	.dbg_req_halt               (hart_req_halt),
+	.dbg_req_halt_on_reset      (hart_req_halt_on_reset),
+	.dbg_req_resume             (hart_req_resume),
+	.dbg_halted                 (hart_halted),
+	.dbg_running                (hart_running),
+	
+	// Debugger access to data0 CSR
+	.dbg_data0_rdata            (hart_data0_rdata),
+	.dbg_data0_wdata            (hart_data0_wdata),
+	.dbg_data0_wen              (hart_data0_wen),
+	
+	// Debugger instruction injection
+	.dbg_instr_data             (hart_instr_data),
+	.dbg_instr_data_vld         (hart_instr_data_vld),
+	.dbg_instr_data_rdy         (hart_instr_data_rdy),
+	.dbg_instr_caught_exception (hart_instr_caught_exception),
+	.dbg_instr_caught_ebreak    (hart_instr_caught_ebreak),
 
-	.dbg_req_halt               (dbg_req_halt_i),
-	.dbg_req_halt_on_reset      (dbg_req_halt_on_reset_i),
-	.dbg_req_resume             (dbg_req_resume_i),
-	.dbg_halted                 (dbg_halted_o),
-	.dbg_running                (dbg_running_o),
-	.dbg_data0_rdata            (dbg_data0_rdata_i),
-	.dbg_data0_wdata            (dbg_data0_wdata_o),
-	.dbg_data0_wen              (dbg_data0_wen_o),
-	.dbg_instr_data             (dbg_instr_data_i),
-	.dbg_instr_data_vld         (dbg_instr_data_vld_i),
-	.dbg_instr_data_rdy         (dbg_instr_data_rdy_o),
-	.dbg_instr_caught_exception (dbg_instr_caught_exception_o),
-	.dbg_instr_caught_ebreak    (dbg_instr_caught_ebreak_o),
-
-	// TODO figure out what this is ? 
-    .dbg_sbus_addr              (32'd0),
-    .dbg_sbus_write             (1'b0),
-    .dbg_sbus_size              (2'h0),
-    .dbg_sbus_vld               (1'b0),
-    .dbg_sbus_rdy               (dbg_sbus_rdy_unused),
-    .dbg_sbus_err               (dbg_sbus_err_unused),
-    .dbg_sbus_wdata             (32'd0),
-    .dbg_sbus_rdata             (dbg_sbus_rdata_unused),
+	// debug access to shared bus 
+	.dbg_sbus_addr              (sbus_addr),
+	.dbg_sbus_write             (sbus_write),
+	.dbg_sbus_size              (sbus_size),
+	.dbg_sbus_vld               (sbus_vld),
+	.dbg_sbus_rdy               (sbus_rdy),
+	.dbg_sbus_err               (sbus_err),
+	.dbg_sbus_wdata             (sbus_wdata),
+	.dbg_sbus_rdata             (sbus_rdata),
 
 	.mhartid_val                (32'd0),
 	.eco_version                (4'd0),
